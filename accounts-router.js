eb512c0bd470324127ca8e50d00284dd417f92f1
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-08 10:55:47 -0700

    .delete added

--- accounts-router.js
+++ accounts-router.js
@@ -54,6 +54,21 @@ router.put('/:id', async (req,res) => {
     } catch (err) {
         res.status(500).json({message:" couldnt find what your looking for", error:err});
     }
-})
+});
+
+router.delete('/:id', async (req,res) => {
+    const {id} = req.params;
+
+    try {
+        const count = await db('accounts').where({id}).del();
+        if (count) {
+            res.status(200).json({deleted: count});
+        } else {
+            res.status(404).json({message: `${id} could not be found`});
+        }
+    } catch (err) {
+        res.status(500).json({message: "could not find what your looking for", error: err });
+    }
+});
 
 module.exports = router;
