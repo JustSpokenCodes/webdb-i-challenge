3fc71edebcf551212ef9e38b50a9496cdd7d19dc
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-08 10:42:27 -0700

    router.get("/:id")

--- accounts-router.js
+++ accounts-router.js
@@ -14,4 +14,19 @@ router.get('/', async (req,res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const [accounts] = await db('accounts').where({id});
+        if (accounts) {
+            res.status(201).json(accounts);
+        } else {
+            res.status(404).json({message:`${id} could not be found in the accounts`});
+        }
+    } catch (err) {
+        res.status(500).json({message: "failed to get the accounts", error:err});
+    }
+});
+
 module.exports = router;
