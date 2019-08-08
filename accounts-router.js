275468bf5a0bf9acafb52830767babf216d1d511
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-08 10:52:22 -0700

    .put added

--- accounts-router.js
+++ accounts-router.js
@@ -40,5 +40,20 @@ router.post('/', async (req,res) => {
     }
 });
 
+router.put('/:id', async (req,res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    try {
+        const count = await db('accounts').where({id}).update(changes);
+        if (count) {
+            res.status(200).json({updated : count});
+        } else {
+            res.status(404).json({message: `${id} account is not found; find it somewhere else`});
+        }
+    } catch (err) {
+        res.status(500).json({message:" couldnt find what your looking for", error:err});
+    }
+})
 
 module.exports = router;
