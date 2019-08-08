0958f97afc8d2d13cc47a885df0d7b4adb49e24c
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-08 10:27:12 -0700

    adding fuctionality and the first get

---
+++ accounts-router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const db = require('./data/dbConfig.js');
+
+const router = express.Router();
+
+router.get('/', async (req,res) => {
+    try {
+        const accounts = await db('accounts');
+
+        res.status(200).json(accounts);
+    }catch (err) {
+        res.status(500).json({message: "There arent any accounts here", error:err});
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
+const AccRouter = require('./accounts-router.js');
+
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccRouter);
+
+server.get('/', (req,res) => {
+    res.send('<h2> DB This is How We Do Accounts<h2>');
+});
 
 module.exports = server;
